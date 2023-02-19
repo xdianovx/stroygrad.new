d6e33f6997e839ad4e5abb36c217899514aa0298
Евгений Дианов <dianov.js@gmail.com>
2023-02-19 21:34:07 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/i,
